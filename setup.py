3071898ad92b49fbe24a8a113b1dc11622560018
Eric Torstenson <eric.s.torstenson@vumc.org>
2020-10-21 16:44:14 -0500

    Added support for versioning

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 
 from ncpi_fhir_utility.config import FHIR_VERSION
 
+from fhir_walk import __version__
+
 root_dir = os.path.dirname(os.path.abspath(__file__))
 req_file = os.path.join(root_dir, "requirements.txt")
 with open(req_file) as f:
@@ -10,6 +12,7 @@ with open(req_file) as f:
 
 setup(
     name="fhir-walk",
+    version = __version__,
     description=f"AnVIL FHIR Walker {FHIR_VERSION}",
     packages=find_packages(),
     include_package_data=True,
